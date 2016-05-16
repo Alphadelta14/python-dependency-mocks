5eea6865d106bd5fc35d01ba3c902bcc2590a2b5
Alphadelta14 <alpha@pokesplash.net>
2016-05-16 07:45:58 -0400

    Initialized tree structure and setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+"""
+dependency-mocks
+
+"""
+
+from setuptools import setup
+
+setup(
+    name='dependency-mocks',
+    version='1.0.0',
+    description='Mock dependencies for remote development',
+    url='https://github.com/Alphadelta14/python-dependency-mocks',
+    author='Alphadelta14',
+    author_email='alpha@alphaservcomputing.solutions',
+    license='MIT',
+    install_requires=[],
+    packages=['dependency_mocks'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2'
+    ]
+)
